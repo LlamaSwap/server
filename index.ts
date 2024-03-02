93184f5e001bcf1776038bf9e68a69549108e4e0
0xngmi <80541789+0xngmi@users.noreply.github.com>
2024-03-02 20:26:46 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -22,7 +22,7 @@ const errorHandler = (func:(req:Request, res:Response)=>Promise<any>) => {
   }
 }
 
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
   res.append('Access-Control-Allow-Origin', '*');
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.append('Access-Control-Allow-Headers', 'Content-Type, authorization');
